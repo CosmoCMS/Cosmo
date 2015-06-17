e1eebff4fc6a21804705c54e4dc5a95b3ad06fd4
James <james@mystrata.com>
2015-06-17 14:33:58 +0400

    Fixes #3 so audio works(ish)

--- core/js/directives/csAudio.js
+++ core/js/directives/csAudio.js
@@ -5,16 +5,15 @@
 
 angular.module('cosmo').directive('csAudio', ['Page', '$routeParams', '$rootScope', 'Users', '$sce', function(Page, $routeParams, $rootScope, Users, $sce) {
     return {
-        template: '<audio ng-dblclick="clicked()"><source ng-repeat="file in audioFiles" ng-src="{{file.src}}"></audio>',
+        template: '<audio ng-click="clicked()"><source ng-repeat="file in audioFiles" ng-src="{{file.src}}"></audio>',
         replace: true,
         link: function(scope, elm, attrs) {
 
             if(Page.extras[attrs.csAudio])
                 scope.audioFiles = angular.fromJson(Page.extras[attrs.csAudio]);
-            else if(Users.admin)
-                scope.audioFiles = [{ src: 'core/img/image.svg', type: 'audio' }];
 
-            elm.css('min-height', '50px'); // Only way to make this clickable in Chrome?
+            if(Users.admin)
+                elm.css('min-height', '50px'); // Only way to make this clickable in Chrome?
 
             // Check if user is an admin
             if(Users.admin) {
@@ -27,9 +26,9 @@ angular.module('cosmo').directive('csAudio', ['Page', '$routeParams', '$rootScop
                 };
 
                 // Save edits/selection of the movie
-                scope.$on('choseGalleryFile', function(event, data){
-                    if(data.id === attrs.csMovie){
-                        scope.audioFiles = data.data;
+                scope.$on('choseFile', function(event, data){
+                    if(data.id === attrs.csAudio){
+                        scope.audioFiles = [{ src: data.src }];
                         Page.extras[attrs.csMovie] = scope.audioFiles;
                     }
                 });
