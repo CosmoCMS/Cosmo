c5255d8b4826eb73de5529c576ed40ff92713956
James Dunn <JDDunn9@Gmail.com>
2015-08-17 21:01:32 +0400

    Fixes #384

--- core/js/factories/hooks.js
+++ core/js/factories/hooks.js
@@ -8,8 +8,27 @@ angular.module('cosmo').factory('Hooks', function(){
     // Initialize all hooks
     var imageHooks = [];
     var HTMLHooks = [];
+    var contentHooks = [];
 
     return {
+        // Content directive. Passes the content as a string
+        contentHook: function(hook){
+            contentHooks.push(hook);
+        },
+        contentHookNotify: function(data){
+            var newData;
+            angular.forEach(contentHooks, function(hook){
+                // Feed the output from the last hook into the next
+                if(newData)
+                    newData = hook(newData);
+                else
+                    newData = hook(data);
+            });
+            if(newData) // Return data after handing it off to any hooks
+                return newData;
+            else
+                return data;
+        },
         // Image directive. Passes the URL of the image as a string
         imageHook: function(hook){
             imageHooks.push(hook);
